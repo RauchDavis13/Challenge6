7ee9ba0ada81a78f39614327a88657d834c064b1
KRD13 <rauchdavis13@gmail.com>
2021-12-12 23:12:43 -0500

    working except for icons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var cityFormEl = document.querySelector("#user-form");
-var savedCityEl = document.querySelector("#saved-cities");
+var savedCityEl = document.getElementById("saved-cities");
 
 var cityInputEl = document.querySelector("#cityName");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -24,6 +24,9 @@ var dailyDateValue;
 // array for saved cities
 var cityHistory = [];
 
+// variable to carry name
+var cityName = "";
+
 displayCity.textContent= ("Your City");
 
 // loads ciyHistory array from local storage at page load/refresh
@@ -35,7 +38,6 @@ window.addEventListener('load', (event) => {
   if(cityRetrive === null) {
     cityHistory = [];
   }
-  console.log(cityHistory);
   citySaved(cityHistory);
   
 });
@@ -46,10 +48,13 @@ var citySaved = function() {
     return    
   } 
   else {
+    // cityName.setAttribute("Id");
+    var cityNameId = 0;
     for (var i = 0; i<=cityHistory.length-1; i++) {
+     
       cityName = cityHistory[i];
+      cityNameId = i + 1;
       cityLoad(cityName);
-      console.log(cityName);
     }
   }
 };
@@ -64,29 +69,31 @@ var citySave = function(cityName) {
   }
   cityHistory.push(cityName);
   localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
-  //citySaved(cityName);
-  console.log(cityHistory);
   cityLoad(cityName);
 };
 
 // creates buttons from saved cities
 var cityLoad = function(cityName) {
   var savedEl = document.createElement("button");
-  //savedEl.classList (justify-content-center);
+  var buttonId = 0;
+
+  // counts # of child elements to created button id
+  if(document.getElementById("saved-cities").childElementCount >0)  {   
+    var x = document.getElementById("saved-cities").childElementCount;
+    console.log(x);
+    buttonId = x + 1;  }
+  else {
+    buttonId = 1;    
+  }
+
+  savedEl.id = "idBtn" + buttonId;
   savedEl.textContent = cityName;
   savedEl.className = "btn2";
-  console.log(savedEl.textContent);
   savedCityEl.appendChild(savedEl);
-
 };
 
 
 displayCity.textContent= ("Your City");
-
-console.log(cityHistory);
-console.log(localStorage);
-
-
 
 // pulls weather criteria from cityName lat and lon properties
 var fetchWeather = function(cityCoord) {  
@@ -120,16 +127,35 @@ var fetchWeather = function(cityCoord) {
     var dateCode = weatherCriteria.currentDate;  
     var dateString = new Date(dateCode * 1000).toLocaleDateString("en-US");
 
-    console.log(weatherCriteria.currentDate);
+    console.log(weatherCriteria.weatherIcon);
 
-    var iconGet = "http://openweathermap.org/img/wn/" + weatherCriteria.weatherIcon +"2x.png";
+    var iconCurrent = weatherCriteria.weatherIcon;
+    var iconCurrentUrl = "http://openweathermap.org/img/wn/" + iconCurrent + "@2x.png";
+    
+    var iconCurrentEl = document.querySelector("#currentIcon");
+
+    var iconCurrentCreate = document.createElement("i");
+    iconCurrentCreate.src = ("iconCurrentUrl");
+    iconCurrentCreate.alt = ("current weather icon");
+   
+    iconCurrentEl.appendChild(iconCurrentCreate);
+    console.log(iconCurrentCreate);
+  
+    console.log(iconCurrentEl);
+    iconCurrentEl.textContent = "";
+    console.log(iconCurrentEl.textContent);
+    iconCurrentEl.textContent = (iconCurrentUrl);
+    //console.log(iconCurrentString);
+    //iconCurrentEl.append(iconCurrentString);
+    console.log(iconCurrentEl);
+    console.log(document.getElementById("currentIcon"));
         
 
     // displays selected city name, current date and weather icon in main area
-    displayCity.textContent = (nameCity + " (" + dateString + ")");
-    currentDateDisplay.textContent = (dateString);
-    current_icon.textContent = (iconGet);
-    console.log(iconGet);
+    displayCity.textContent = (nameCity);
+    currentDateDisplay.textContent = (" (" + dateString + ") ");
+    // current_icon.textContent = (iconGet);
+    // console.log(iconGet);
     console.log(current_icon);       
     
     console.log(weatherCriteria.weatherIcon);
@@ -147,11 +173,11 @@ var fetchWeather = function(cityCoord) {
       for(var i = 0; i<=5; i++ ) {
         dayCount = i;
         var dailyWeather = {
-          dailyIcon: data.daily[i + 1].weather[0].icon,
           dailyDate: data.daily[i + 1].dt,
-          dailyHumidity: data.daily[i + 1].humidity,
+          dailyIcon: data.daily[i + 1].weather[0].icon,
           dailyTemp: data.daily[i + 1].temp.day,
-          dailyWind: data.daily[i + 1].wind_speed
+          dailyWind: data.daily[i + 1].wind_speed,
+          dailyHumidity: data.daily[i + 1].humidity
         }
 
         // handles turning unix timestamp into readable date
@@ -160,18 +186,21 @@ var fetchWeather = function(cityCoord) {
 
         // using 'for loop' to add "i" + 1 to string to define ID name
         var dateDaily = i + 1 + "Date";
+        var iconDaily = i + 1 + "Icon";
         var tempDaily = i + 1 + "Temp";
         var windDaily = i + 1 + "Wind";
         var humidDaily = i + 1 + "Humidity";
-                
+
         // connects to daily weather data containers
         var daily_date = document.getElementById(dateDaily);
+        var daily_icon = document.getElementById(iconDaily);
         var daily_temp = document.getElementById(tempDaily);
         var daily_humidity = document.getElementById(humidDaily);
         var daily_wind = document.getElementById(windDaily);
 
         // loads daily weather containers
         daily_date.textContent = (dateString);
+        daily_icon.textContent = (dailyWeather.dailyIcon);
         daily_temp.textContent = (dailyWeather.dailyTemp);
         daily_wind.textContent = (dailyWeather.dailyWind);
         daily_humidity.textContent = (dailyWeather.dailyHumidity);
@@ -190,7 +219,7 @@ var formSubmitHandler = function(event) {
   event.preventDefault();
 
   // get value(city name) from input element
-  var cityName = cityInputEl.value.trim();
+  cityName = cityInputEl.value.trim();
   
   // sends cityName to both fetchLocation and citySave functions
   if (cityName) {
@@ -253,3 +282,9 @@ var buttonClickHandler = function(event) {
 
 // add event listeners to form and button container
 cityFormEl.addEventListener("submit", formSubmitHandler);
+
+savedCityEl.addEventListener("click", (event) => {
+  var buttonText = event.target.textContent;
+  cityName = buttonText;
+  fetchLocation(cityName);
+});
